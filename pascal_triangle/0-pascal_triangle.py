19a1c83630f3e77327761c37396a8ea4891c3260
Anthodido <anthony.didomenico678@gmail.com>
2026-05-11 10:22:19 +0200

    Pascal_Triangle

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    triangle = []
+    for j in range (1, n +1):
+        row = [1] * j
+        for i in range(1, j - 1):
+            row[i] = triangle[j - 2][i - 1] + triangle[j - 2][i]
+        triangle.append(row)
+    return triangle
