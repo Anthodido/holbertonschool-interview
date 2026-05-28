4cd473e47dff887cc51d20f362a1191877907386
Anthodido <anthony.didomenico678@gmail.com>
2026-05-28 14:28:41 +0200

    0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -4,12 +4,24 @@
 
 
 def minOperations(n):
-    """calculates the fewest number of operations needed to result"""
+    """return the min number of operations
+    Args:
+        n (int): target number H
+    Returns:
+            int: min number
+    """
     if n <= 1:
         return 0
+
     operations = 0
-    for i in range(2, n + 1):
-        while n % i == 0:
-            operations += i
-            n //= i
+    current_length = 1
+
+    while current_length < n:
+        if n % current_length == 0:
+            operations += 1
+            current_length *= 2
+        else:
+            operations += 1
+            current_length += 1
+
     return operations
