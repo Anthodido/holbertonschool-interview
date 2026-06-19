917ad6cd189851980ad78a234b12aae251c8ed77
Anthodido <anthony.didomenico678@gmail.com>
2026-06-19 09:44:46 +0200

    queen

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -1,2 +1,49 @@
 #!/usr/bin/python3
 
+#!/usr/bin/python3
+"""Solves the N queens problem using backtracking."""
+import sys
+
+
+def is_safe(board, row, col):
+    """Check if placing a queen at (row, col) is safe."""
+    for i in range(row):
+        if board[i] == col:
+            return False
+        if abs(board[i] - col) == abs(i - row):
+            return False
+    return True
+
+
+def solve(board, row, n, solutions):
+    """Recursively place queens row by row using backtracking."""
+    if row == n:
+        solutions.append([[i, board[i]] for i in range(n)])
+        return
+    for col in range(n):
+        if is_safe(board, row, col):
+            board[row] = col
+            solve(board, row + 1, n, solutions)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [0] * n
+    solutions = []
+    solve(board, 0, n, solutions)
+
+    for solution in solutions:
+        print(solution)
