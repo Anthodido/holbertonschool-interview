5a454cb307327a15214143123903d4ec879051be
Anthodido <anthony.didomenico678@gmail.com>
2026-07-01 10:18:42 +0200

    fix syntax error line 19

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -16,5 +16,5 @@ def rotate_2d_matrix(matrix):
         for i in range(j, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
     
-    for i inr range[n]:
+    for i in range(n):
         matrix[i].reverse()
