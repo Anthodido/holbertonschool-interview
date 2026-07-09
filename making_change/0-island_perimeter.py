32cc96d875dc9937f77e609f83470078eddf4099
Anthodido <anthony.didomenico678@gmail.com>
2026-07-09 09:44:08 +0200

    0-island_perimeter.py

---
+++ making_change/0-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of an island in a grid.
+
+    Args:
+        grid (list of list of int): A 2D grid representing the map, where 1 represents land and 0 represents water.
+
+    Returns:
+        int: The perimeter of the island.
+    """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                # Check the four possible directions (up, down, left, right)
+                if i == 0 or grid[i - 1][j] == 0:  # Up
+                    perimeter += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:  # Down
+                    perimeter += 1
+                if j == 0 or grid[i][j - 1] == 0:  # Left
+                    perimeter += 1
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:  # Right
+                    perimeter += 1
+    return perimeter
