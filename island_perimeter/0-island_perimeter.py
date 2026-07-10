419a39149f1832d25a083972d1cb33f884553b3f
Anthodido <anthony.didomenico678@gmail.com>
2026-07-10 14:43:50 +0200

    0-island_perimeter.py

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Module for computing the perimeter of an island."""
 
 
 def island_perimeter(grid):
@@ -13,22 +14,20 @@ def island_perimeter(grid):
     Returns:
         int: The perimeter of the island.
     """
-if not grid or not grid[0]:
+    if not grid or not grid[0]:
         return 0
 
     perimeter = 0
     rows = len(grid)
     cols = len(grid[0])
 
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == 1:
-                if i == 0 or grid[i - 1][j] == 0:
-                    perimeter += 1
-                if i == len(grid) - 1 or grid[i + 1][j] == 0:
-                    perimeter += 1
-                if j == 0 or grid[i][j - 1] == 0:
-                    perimeter += 1
-                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
-                    perimeter += 1
+    for i in range(rows):
+        for j in range(cols):
+            if grid[rows][cols] == 1:
+                perimeter += 4
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+
     return perimeter
